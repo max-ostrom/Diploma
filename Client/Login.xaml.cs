44d36078185cfad1e833670c78726575a81cf254
Максим Остромецкий <max.ostrom@gmail.com>
2019-04-23 21:37:04 +0300

    Add project files.

---
+++ Client/Login.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Client
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Not implemented yet","Error",
+         MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+    }
+}
